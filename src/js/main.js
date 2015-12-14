d552728abe54af9787bd7cc83663ad7dcf153420
disoul <starky_ds@sina.cn>
2015-12-15 01:32:49 +0800

    add webpack

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+require('../../bower_components/bootstrap/dist/css/bootstrap.css');
+require('../../bower_components/bootstrap/dist/css/bootstrap-theme.css');
+require('../sass/index.scss');
+
+require('bootstrap');
+require('jquery');
