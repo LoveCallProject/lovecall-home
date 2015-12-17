65f4ae33e11d2746eac4de651d7f8e41de101dbc
disoul <starky_ds@sina.cn>
2015-12-17 23:45:30 +0800

    use hanayo to build

---
+++ article/feedback.md
@@ -0,0 +1,12 @@
+---
+title: 意见反馈
+author: Disoul
+tag:
+    - 公告
+date: 2015/12/17 23:11
+picture: /public/images/shi_ka_co.jpg
+---
+
+・8・:　把你们变成bug的点心哦
+
+请为我们提出宝贵的建议
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-require('../../bower_components/bootstrap/dist/css/bootstrap.css');
-require('../../bower_components/bootstrap/dist/css/bootstrap-theme.css');
-require('../sass/index.scss');
-
-require('bootstrap');
-require('jquery');
